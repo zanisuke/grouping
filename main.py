e86777aa56048d7b53bd42346f4570f8e9c1f10f
KawaguchiKyohei <kk@KawaguchiKyoheinoMacBook-Pro.local>
2020-06-04 22:23:12 +0900

    update direct action

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-import math,random
+import math
+import random
 import discord
 
-
-TOKEN = 'NzE3NzMzMjE4MzEyMjU3NTY3.Xteniw.VAJi41wKYuH9uV9h8xJlnSsppsg'
+TOKEN = ''
 
 message_id = ''
 member_cache = []
 
-group_header_list = ['A','B','C','D','E','F','G','H','I','J','K']
+reaction_message_id = ''
+is_reaction_method = False
+
+group_header_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
 
 client = discord.Client()
+
 
 async def grouping(member, num, channel):
     member_count = len(member)
     if member_count < num:
         await channel.send('人数に対してグループ数が多すぎます')
         return
-    
+
     group_count = num
     group_number = math.ceil(member_count / num)
     random.shuffle(member)
@@ -39,7 +43,6 @@ async def grouping(member, num, channel):
 
     global member_cache
     member_cache = member
-    
 
 
 @client.event
@@ -49,44 +52,35 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
 @client.event
 async def on_raw_reaction_add(payload):
-    ch = client.get_channel(payload.channel_id)
-    await ch.send("reaction!")
-    await ch.send(message_id)
-    await ch.send(payload.message_id)
-    if (message_id == ''):
+    if not is_reaction_method:
+        return
+    if payload.message_id != reaction_message_id:
         return
 
-    if payload.message_id == message_id:
-        ch = client.get_channel(payload.channel_id)
-        await ch.send("sample!")
-        
 
-# @client.event
-# async def on_raw_reaction_remove(payload):
-#     if payload.message_id == ID:
-#         print(payload.emoji.name)
+@client.event
+async def on_raw_reaction_remove(payload):
+    if not is_reaction_method:
+        return
+    if payload.message_id != reaction_message_id:
+        return
 
-#         guild_id = payload.guild_id
-#         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-#         role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
-
-#         if role is not None:
-#             member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-#             await member.remove_roles(role)
-#             print("done")
 
 @client.event
 async def on_message(message):
+    global is_reaction_method
     if message.author == client.user:
         return
-    
+
     split = message.content.split(' ')
     trigger = split[0]
     if trigger == 'grouping':
         grouping_method = split[1] if split[1:2] else ''
-        grouping_number = int(split[2]) if split[2:3] and split[2].isdecimal() else ''
+        grouping_number = int(
+            split[2]) if split[2:3] and split[2].isdecimal() else ''
 
         if not grouping_method or not grouping_number:
             await message.channel.send('入力が正しくありません')
@@ -96,7 +90,11 @@ async def on_message(message):
             member = split[3].split(',')
             await grouping(member, grouping_number, message.channel)
         elif grouping_method == 'reaction':
-            await message.channel.send('リアクショングルーピング')
+            global reaction_message_id
+            is_reaction_method = True
+            mes = 'リアクションしたメンバーを{0}チームに分けます\n参加する人は:thumbsup:でリアクションしてください'
+            mes = mes.format(grouping_number)
+            reaction_message_id = await message.channel.send(mes).id
         elif grouping_method == 'channel':
             await message.channel.send('チャンネルグルーピング')
         elif grouping_method == 're':
